860c8b5b91bb91ff5362ea2b6b9bd89152b64ab2
SunilOS <sunilos007@gmail.com>
2020-01-26 06:06:18 +0530

    Init Version

---
+++ ORS/src/main/java/com/sunilos/ctl/College.java
@@ -0,0 +1,43 @@
+package com.sunilos.ctl;
+
+public class College {
+
+	private long id = 0;
+	private String name = null;
+	private String address = null;
+
+	public College() {
+
+	}
+
+	public College(long id, String name, String address) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
---
+++ ORS/src/main/java/com/sunilos/ctl/CollegeCtl.java
@@ -0,0 +1,93 @@
+package com.sunilos.ctl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "College")
+public class CollegeCtl {
+
+	private static HashMap<Long, College> data = new HashMap<Long, College>();
+
+	static {
+		data.put(1L, new College(1, "IIT", "Mumbai"));
+		data.put(2L, new College(2, "IIM", "Delhi"));
+		data.put(3L, new College(3, "AIMS", "Delhi"));
+	}
+
+	@GetMapping("get/{id}")
+	public Map<String, Object> get(@PathVariable Long id) {
+
+		System.out.println("ID: " + id);
+
+		Map<String, Object> response = new HashMap<String, Object>();
+
+		College c = data.get(id);
+
+		if (c != null) {
+			response.put("success", true);
+			response.put("data", c);
+		} else {
+			response.put("success", false);
+			response.put("message", "College not found");
+		}
+
+		return response;
+	}
+
+	@PostMapping("save")
+	public Map<String, Object> save(@RequestBody College c) {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+
+		Long id = c.getId();
+		
+		System.out.println("Received ID " + id);
+		
+		if (id > 0) {
+			data.remove(id);
+			data.put(id, c);
+		} else {
+			c.setId(data.size()+1);
+			data.put(id, c);
+		}
+		response.put("success", true);
+		response.put("message", "Data is saved");
+		response.put("data", c);
+		return response;
+	}
+
+	@GetMapping("delete/{id}")
+	public Map<String, Object> delete(@PathVariable Long id) {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+
+		College c = data.get(id);
+
+		if (c == null) {
+			response.put("success", false);
+			response.put("message", "College not found");
+		} else {
+			data.remove(id);
+			response.put("success", true);
+			response.put("data", c);
+		}
+		return response;
+	}
+
+	@PostMapping("search")
+	public Map<String, Object> search(@RequestBody College c) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("success", true);
+		response.put("data", data);
+		return response;
+	}
+
+}
